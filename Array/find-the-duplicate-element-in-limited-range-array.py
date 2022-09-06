e9ba7e523a7b65540af1b537b4906c94010fa028
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-09-06 19:15:49 +0430

    find duplicate O(n)

--- Array/find-the-duplicate-element-in-limited-range-array.py
+++ Array/find-the-duplicate-element-in-limited-range-array.py
@@ -4,3 +4,16 @@ def findDuplicate(ls):
             if ls[j]==ls[i]:
                 print('the duplcate is: ',ls[i])
                 return
+def findDup(ls):
+    d={}
+
+    for i, e in enumerate(ls):
+        d[e]=i
+    print(d)
+    compLs=[i for i in range(len(ls))]
+    print(compLs)
+    for key, value in d.items():
+        compLs.remove(value)
+    if compLs.__len__()!=0:
+        print('the duplicate is: ',ls[compLs[0]])
+findDup([1,100,100,3,4,5,6])
