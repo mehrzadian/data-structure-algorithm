5b5211c39ac99f99945cad56ad5b08c8e5198fb1
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-09-08 16:06:26 +0430

    sort with swapping

--- Array/sort-array-of-0's-1's-2's.py
+++ Array/sort-array-of-0's-1's-2's.py
@@ -11,4 +11,24 @@ def sort(ls):
         else:
             twos+=1
     return [0]*zeros+[1]*ones+[2]*twos
-print(sort([1,1,1,0,0,1,0]))
+print(sort(ls:=[1,1,2,1,0,0,1,2,2,0]))
+#threeWayPartioning
+def swap(ls,a,b):
+    temp=ls[a]
+    ls[a]=ls[b]
+    ls[b]=temp
+def pivotSort(ls):
+    start=mid=0
+    end=len(ls)-1
+    pivot = 1
+    while mid<=end:
+        if ls[mid]<pivot:
+            swap(ls,start,mid)
+            start+=1
+            mid+=1
+        elif ls[mid]>pivot:
+            swap(ls,end,mid)
+            end-=1
+        else:
+            mid+=1
+print(sort(ls))
