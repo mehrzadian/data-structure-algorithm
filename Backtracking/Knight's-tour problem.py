dbdb37365797e930d22c39523b793554b97a07be
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-10-21 12:07:07 +0330

    knght's tour 2

--- Backtracking/Knight's-tour problem.py
+++ Backtracking/Knight's-tour problem.py
@@ -5,17 +5,47 @@
 n = int(input())
 
 
-
 def issafe(x, y, board):
     if 0 <= x < n and 0 <= y < n and board[x][y] == -1:
         return True
     return False
+
+
 def printSolution(n, board):
     for i in range(n):
         for j in range(n):
-            print(board[i][j],end=" ")
+            print(board[i][j], end=" ")
         print()
+
 
 def solveKT(n):
     board = [[-1 for x in range(8)] for i in range(n)]
-    if ==0:
+    moveX = [1, 2, 1, 2, -1, -2, -1, -2]
+    moveY = [2, 1, -2, -1, 2, 1, -2, -1]
+    board[0][0]=0
+    pos=1
+    if solveKTUtil(n, board, 0, 0, moveX, moveY, pos):
+        printSolution(n, board)
+    else:
+        print('solution does\'nt exist')
+
+def solveKTUtil(n, board, currX ,currY, moveX, moveY,pos):
+    if pos==n*n:
+        return True
+
+    for i in range(8):
+        newX = currX + moveX[i]
+        newY = currY + moveY[i]
+        if issafe(newX, newY, board):
+            currX=newX
+            currY = newY
+            board[newX][newY] = pos
+            pos+=1
+            if solveKTUtil(n, board, currX,currY,moveX,moveY,pos):
+                return True
+            board[newX][newY]=-1
+    return False
+
+
+
+
