33670fd65faa4e0965b2a8d9e36abf39ae822879
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-10-28 17:04:39 +0330

    Update array rotation.py

--- Array/array rotation.py
+++ Array/array rotation.py
@@ -1,5 +1,5 @@
 # https://www.geeksforgeeks.org/array-rotation/
-
+# https://www.geeksforgeeks.org/program-for-array-rotation-continued-reversal-algorithm/
 # n=int(input())
 rotation=int(input())
 ls = list(map(int, input().split()))
