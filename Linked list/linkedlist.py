06280731bc5ee0432e2f601ef82879f849ee12e0
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-10-28 17:06:34 +0330

    linkedlist 2

--- Linked list/linkedlist.py
+++ Linked list/linkedlist.py
@@ -4,3 +4,16 @@ class Node:
     def __init__(self, data):
         self.data = data  # Assign data
         self.next = None  # Initialize next as null
+# Linked List class contains a Node object
+class LinkedList:
+
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    def printll(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
