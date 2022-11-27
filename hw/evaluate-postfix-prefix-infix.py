daa523eb41c253810f8d7f179d9ecb92f5ec9be0
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-11-27 10:06:58 +0330

    postfix evaluation in evaluate-postfix-prefix-infix.py

--- hw/evaluate-postfix-prefix-infix.py
+++ hw/evaluate-postfix-prefix-infix.py
@@ -1,37 +1,68 @@
-print(7//3)
+print(7 // 3)
 kind = int(input("if postfix:-1, infix:0, prefix:1\n"))
 str = input()
-#evaluate if prefix or postfix
+
+
+# evaluate if prefix or postfix
 
 def postfixEvaluation(str):
-    stack=[]
-    operators ={'+','-','*', '/'}
+    stack = []
+    operators = {'+', '-', '*', '/'}
     for i in str:
         if i.isnumeric():
             stack.append(int(i))
         elif i in operators:
-            a = stack.pop()
-            b = stack.pop()
-
-            match i:
-                case '+':
-                    result = a+b
-                case '-':
-                    result = a-b
-                case '/':
-                    result = b//a
-                case '*':
-                    result = a*b
-                case '^':
-                    result = a**b
-            stack.append(result)
+            operation(-1,i, stack)
         else:
             return "unacceptable value!"
     return stack.pop()
 
 
+def operation(kind,i, stack):
+    a = stack.pop()
+    b = stack.pop()
+    if kind==-1:
+        match i:
+            case '+':
+                result = a + b
+            case '-':
+                result = a - b
+            case '/':
+                result = b // a
+            case '*':
+                result = a * b
+            case '^':
+                result = a ** b
+    else:
+        match i:
+            case '+':
+                result = a + b
+            case '-':
+                result = a - b
+            case '/':
+                result = a // b
+            case '*':
+                result = a * b
+            case '^':
+                result = a ** b
+    stack.append(result)
+
+
+def prefixEvaluation(str):
+    stack = []
+    operators = {'+', '-', '*', '/'}
+
+    for i in str[::-1]:
+        if i.isnumeric():
+            stack.append(int(i))
+        elif i in operators:
+            operation(1,i, stack)
+        else:
+            return "unacceptable value!"
+    return stack.pop()
+
 
 if kind == -1:
     print(postfixEvaluation(str))
-# else:
-#     prefixEvaluation(str)
+else:
+    print(prefixEvaluation(str))
