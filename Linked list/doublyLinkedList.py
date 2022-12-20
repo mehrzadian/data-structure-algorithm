d747b37aed2a4e68c186e5cb4a229e2db28c52a3
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-12-20 10:23:26 +0330

    doublyLinkedList.py

---
+++ Linked list/doublyLinkedList.py
@@ -0,0 +1,137 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    def pushAtFirst(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+    def pushAtEnd(self, data):
+        node = Node(data)
+        if self.head == None:
+            self.head = node
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+            temp.next = node
+            node.prev = temp
+    def deleteAtFirst(self):
+        temp = self.head
+        self.head = self.head.next
+        self.head.prev = None
+        del temp
+    def deleteAtEnd(self):
+        temp = self.head
+        prev = None
+        if temp == None:
+            print('empty')
+        while temp.next:
+            prev = temp
+            temp = temp.next
+        prev.next = None
+    def search(self, data):
+        temp = self.head
+        while temp:
+            if temp.data == data:
+                return True
+            temp = temp.next
+        return False
+    def searchRecursively(self, head, data):
+        if not head:
+            return False
+        if head.data == data:
+            return True
+        return self.searchRecursively(head.next, data)
+    def print(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+    def printReverse(self):
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.prev
+        print()
+    def isEmpty(self):
+        return self.head == None
+    def size(self):
+        temp = self.head
+        count = 0
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
+    def clear(self):
+        self.head = None
+    def __str__(self):
+        temp = self.head
+        string = ''
+        while temp:
+            string += str(temp.data) + ' '
+            temp = temp.next
+        return string
+    def delete(self):
+        del self.head
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp.data
+            temp = temp.next
+    def __len__(self):
+        return self.size()
+    def __getitem__(self, index):
+        if index >= self.size():
+            raise IndexError('index out of range')
+        temp = self.head
+        for i in range(index):
+            temp = temp.next
+        return temp.data
+    def __setitem__(self, index, data):
+        if index >= self.size():
+            raise IndexError('index out of range')
+        temp = self.head
+        for i in range(index):
+            temp = temp.next
+        temp.data = data
+    def __delitem__(self, index):
+        if index >= self.size():
+            raise IndexError('index out of range')
+        if index == 0:
+            self.deleteAtFirst()
+        else:
+            temp = self.head
+            for i in range(index):
+                temp = temp.next
+            temp.prev.next = temp.next
+            temp.next.prev = temp.prev
+            del temp
+    def __contains__(self, data):
+        return self.search(data)
+    def __reversed__(self):
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        while temp:
+            yield temp.data
+            temp = temp.prev
+    def __add__(self, other):
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = other.head
+        other.head.prev = temp
+        return self
+    
