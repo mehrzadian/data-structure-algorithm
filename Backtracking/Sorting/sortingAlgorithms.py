0eb075217353956978c311631ae2136aa3159058
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-12-27 11:12:01 +0330

    binaryInsertionSort

--- Backtracking/Sorting/sortingAlgorithms.py
+++ Backtracking/Sorting/sortingAlgorithms.py
@@ -1,3 +1,4 @@
+
 class sortingAlgorithms:
     def selectionSort(arr):
         for i in range(len(arr)-1):
@@ -17,4 +18,40 @@ class sortingAlgorithms:
                 j-=1
                 curr-=1
         return arr
+
+    #from GeeksForGeeks
+    def binarySearch(arr, val, start, end):
+        
+        if start == end:
+            if arr[start] > val:
+                return start
+            else:
+                return start+1
+    
+        
+        if start > end:
+            return start
+    
+        mid = (start+end)//2
+        if arr[mid] < val:
+            return sortingAlgorithms.binarySearch(arr, val, mid+1, end)
+        elif arr[mid] > val:
+            return sortingAlgorithms.binarySearch(arr, val, start, mid-1)
+        else:
+            return mid
+  
+    #from GfG
+    def insertionSort(arr):
+        for i in range(1, len(arr)):
+            val = arr[i]
+            j = sortingAlgorithms.binarySearch(arr, val, 0, i-1)
+            print(i,"  ",j)
+            arr = arr[:j] + [val] + arr[j:i] + arr[i+1:]
+            print("arr : ", arr)
+        return arr
+  
+  
+
+
+        
 print(sortingAlgorithms.insertionSort([64,12,23,11,22]))
