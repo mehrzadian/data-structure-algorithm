b2b5785fc16276b84d1007b4918dc8cccc32b0ca
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2023-01-01 13:18:33 +0330

    improved merge sort, while deleted

--- Backtracking/Sorting/sortingAlgorithms.py
+++ Backtracking/Sorting/sortingAlgorithms.py
@@ -81,21 +81,13 @@ class sortingAlgorithms:
             sortingAlgorithms.mergeSort(R)
 
             i=j=k=0
-            while i<len(L) and j<len(R):
-                if L[i]<=R[j]:
+            while k<len(arr):
+                if j>=len(R) or (i<len(L) and L[i]<=R[j]):
                     arr[k]=L[i]
                     i+=1
                 else:
                     arr[k]=R[j]
                     j+=1
-                k+=1
-            while i<len(L):
-                arr[k]=L[i]
-                i+=1
-                k+=1
-            while j<len(R):
-                arr[k]=R[j]
-                j+=1
                 k+=1
         return arr
             
--- Backtracking/Sorting/tempCodeRunnerFile.py
+++ Backtracking/Sorting/tempCodeRunnerFile.py
@@ -1,3 +1,8 @@
-r,low,high):
-    #     partition(arr)
-    #     sortingAlgorith
+while i<len(L):
+            #     arr[k]=L[i]
+            #     i+=1
+            #     k+=1
+            # while j<len(R):
+            #     arr[k]=R[j]
+            #     j+=1
+            #     k+=1
