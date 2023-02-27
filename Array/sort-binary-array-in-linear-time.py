b31371f141cb5da9ff816d85bb35dae8bfe0694e
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2023-02-27 17:12:43 +0330

    sort binary array in linear time

--- Array/sort-binary-array-in-linear-time.py
+++ Array/sort-binary-array-in-linear-time.py
@@ -1,10 +1,6 @@
-def sort(ls):
-    zeros=[]
-    ones=[]
-    for i in range(len(ls)):
-        if ls[i] == 0:
-            zeros.append(0)
-        else:
-            ones.append(1)
-    return zeros+ones
-print(sort([0,1,0,1,0,1,1,1,0,0,0]))
+import sys
+sys.path.append('greedy')
+
+
+start=[10,20,30]
+end = [100,30,40]
